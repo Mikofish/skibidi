67f97025bb89c5ee84a8ef64caed5ddfff2f4128
Mikofish <214334@clvsc.tyc.edu.tw>
2025-02-13 00:45:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello, Flask on Vercel!"
-
-def handler(event, context):
-    return app(event, context)
+    return "Hello, Flask!"
+if __name__ == '__main__':
+    app.run(port:5050)
