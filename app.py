87b3b3f213e49230da33663f371aa9b7e3c4a0a7
Mikofish <214334@clvsc.tyc.edu.tw>
2025-02-13 14:32:54 +0800

    V5

--- app.py
+++ app.py
@@ -1,56 +1,42 @@
-from flask import Flask, request, abort, render_template
+from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import os
 
-
 app = Flask(__name__)
 
-# # 環境變數（Vercel 會自動讀取）
+# 設定 LINE Channel Access Token 和 Secret
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
 LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 line_handler = WebhookHandler(LINE_CHANNEL_SECRET)
-
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template('index.html')
-@app.route('/why')
-def why():
-    return render_template('why.html')
+    return "LINE BOT 首頁"
 
-@app.route('/trainer')
-def trainer():
-    return render_template('trainer.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-@app.route("/callback", methods=['POST'])
+@app.route("/callback", methods=["POST"])
 def callback():
-    signature = request.headers['X-Line-Signature']
+    signature = request.headers["X-Line-Signature"]
     body = request.get_data(as_text=True)
-
-    app.logger.info("Request body: " + body)
 
     try:
         line_handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
 
-    return 'OK'
+    return "OK"
 
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    """回覆與使用者相同的訊息"""
+    user_message = event.message.text
+    reply_message = f"你說了：{user_message}"
+    
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)
+        TextSendMessage(text=reply_message)
     )
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=8000)
